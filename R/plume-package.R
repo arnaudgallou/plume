1f25e71c89161715d02b54d8ca3ed55bc8ad97ce
Arnaud Gallou <67476099+arnaudgallou@users.noreply.github.com>
2024-08-15 17:11:45 +0200

    Upkeep (#79)
    * Use roxygen2 7.3.1
    
    * Remove package description from plume-package.R
    
    * Rebuild plume-package.R with usethis
    
    * Update file name in `.Rd`
    
    * `usethis::use_covr_ignore()`
    
    * Update snapshots with latest knitr version

--- R/plume-package.R
+++ R/plume-package.R
@@ -1,28 +1,75 @@
-#' @description Handles and formats author information in scientific writing in
-#'   'R Markdown' and 'Quarto'. 'plume' provides easy-to-use and flexible tools
-#'   for injecting author metadata in 'YAML' headers as well as generating
-#'   author and contribution lists (among others) as strings from tabular data.
 #' @keywords internal
-#' @importFrom dplyr distinct filter select dense_rank
-#' @importFrom dplyr mutate summarise across rename arrange
-#' @importFrom dplyr if_else if_any
-#' @importFrom tidyr drop_na pivot_longer nest unnest
-#' @importFrom tidyselect all_of any_of starts_with
-#' @importFrom tibble tibble as_tibble_row as_tibble rowid_to_column
-#' @importFrom purrr partial reduce
-#' @importFrom purrr map map_vec map2_vec iwalk
-#' @importFrom purrr list_rbind list_transpose list_assign list_modify
-#' @importFrom readr read_file write_lines
-#' @importFrom rlang %||% := abort set_names
-#' @importFrom rlang enexprs sym syms
-#' @importFrom rlang is_named have_name is_string is_bool is_true
-#' @importFrom rlang caller_env current_env caller_arg
-#' @importFrom glue glue glue_collapse
-#' @importFrom vctrs vec_group_id vec_duplicate_any vec_restore vec_rank
-#' @importFrom stringr str_split_1 str_remove_all str_replace str_replace_all
-#' @importFrom stringr str_extract str_extract_all fixed regex
-#' @importFrom jsonlite toJSON parse_json
-#' @importFrom yaml yaml.load as.yaml
-#' @importFrom R6 R6Class
-#' @importFrom lifecycle deprecated
 "_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom dplyr across
+#' @importFrom dplyr arrange
+#' @importFrom dplyr dense_rank
+#' @importFrom dplyr distinct
+#' @importFrom dplyr filter
+#' @importFrom dplyr if_any
+#' @importFrom dplyr if_else
+#' @importFrom dplyr mutate
+#' @importFrom dplyr rename
+#' @importFrom dplyr select
+#' @importFrom dplyr summarise
+#' @importFrom glue glue
+#' @importFrom glue glue_collapse
+#' @importFrom jsonlite parse_json
+#' @importFrom jsonlite toJSON
+#' @importFrom lifecycle deprecated
+#' @importFrom purrr iwalk
+#' @importFrom purrr list_assign
+#' @importFrom purrr list_modify
+#' @importFrom purrr list_rbind
+#' @importFrom purrr list_transpose
+#' @importFrom purrr map
+#' @importFrom purrr map_vec
+#' @importFrom purrr map2_vec
+#' @importFrom purrr partial
+#' @importFrom purrr reduce
+#' @importFrom R6 R6Class
+#' @importFrom readr read_file
+#' @importFrom readr write_lines
+#' @importFrom rlang :=
+#' @importFrom rlang %||%
+#' @importFrom rlang abort
+#' @importFrom rlang caller_arg
+#' @importFrom rlang caller_env
+#' @importFrom rlang current_env
+#' @importFrom rlang enexprs
+#' @importFrom rlang have_name
+#' @importFrom rlang is_bool
+#' @importFrom rlang is_named
+#' @importFrom rlang is_string
+#' @importFrom rlang is_true
+#' @importFrom rlang set_names
+#' @importFrom rlang sym
+#' @importFrom rlang syms
+#' @importFrom stringr fixed
+#' @importFrom stringr regex
+#' @importFrom stringr str_extract
+#' @importFrom stringr str_extract_all
+#' @importFrom stringr str_remove_all
+#' @importFrom stringr str_replace
+#' @importFrom stringr str_replace_all
+#' @importFrom stringr str_split_1
+#' @importFrom tibble as_tibble
+#' @importFrom tibble as_tibble_row
+#' @importFrom tibble rowid_to_column
+#' @importFrom tibble tibble
+#' @importFrom tidyr drop_na
+#' @importFrom tidyr nest
+#' @importFrom tidyr pivot_longer
+#' @importFrom tidyr unnest
+#' @importFrom tidyselect all_of
+#' @importFrom tidyselect any_of
+#' @importFrom tidyselect starts_with
+#' @importFrom vctrs vec_duplicate_any
+#' @importFrom vctrs vec_group_id
+#' @importFrom vctrs vec_rank
+#' @importFrom vctrs vec_restore
+#' @importFrom yaml as.yaml
+#' @importFrom yaml yaml.load
+## usethis namespace: end
+NULL
