29e8a50a998d083cc4087485e9088fa71c879d7f
arnaudgallou <arnaudgallou31@gmail.com>
2025-09-25 10:30:57 +0200

    Add test

---
+++ tests/testthat/_snaps/as-lines.md
@@ -0,0 +1,8 @@
+# as_lines() gives meaningful error messages
+
+    Code
+      as_lines(list())
+    Condition
+      Error in `as_lines()`:
+      ! `...` must be an atomic vector.
+
--- tests/testthat/test-as-lines.R
+++ tests/testthat/test-as-lines.R
@@ -1,3 +1,12 @@
 test_that("as_lines() prints vector elements on distinct lines", {
   expect_output(as_lines("a", "b"), "a\\R{2}b", perl = TRUE)
 })
+
+# Errors ----
+
+test_that("as_lines() gives meaningful error messages", {
+  expect_snapshot(
+    as_lines(list()),
+    error = TRUE
+  )
+})
